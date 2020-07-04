b4873a39dca30bdcb9460a0815524fd722ef369d
TNORTHERN <traynorthern@yahoo.com>
2020-07-04 09:31:09 -0500

    changing title

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -4,6 +4,9 @@ import HeroText from "../components/HeroText";
 import HeroImage from "../components/HeroImage";
 import Layout from "../components/layout/Layout";
 const Welcome = () => {
+  React.useEffect(() => {
+    document.title = "Champions Round - Landing"
+  }, [])
   return (
     <Layout>
       <div className="container pt-12 mx-auto lg:px-24 xl:px-20 px-8">
