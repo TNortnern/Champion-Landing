b5f17021279a116ad4e63b5ad02e3b0f5d042e9a
TNORTHERN <traynorthern@yahoo.com>
2020-07-04 09:30:16 -0500

    changing title

--- src/components/HeroText.js
+++ src/components/HeroText.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 const HeroText = () => {
+  const url =
+    "https://championsround.us20.list-manage.com/subscribe/post?u=44b2ca89ff52f128d609cb138&amp;id=f58c83fc50";
+const SimpleForm = () => <MailchimpSubscribe url={url} />;
+
     return (
       <div className="w-11/12 text-left sm:text-center lg:text-left lg:w-11/25 xl:w-1/2 pt-4 xl:pt-20">
         <h2 className="text-2xl lg:text-3xl xl:text-5xl uppercase mb-4">
@@ -17,29 +22,48 @@ const HeroText = () => {
         <p className="text-xl mb-5 font-light">
           Join our mailing list for the latest news & updates.
         </p>
-        <form
-          action="https://championsround.us20.list-manage.com/subscribe/post?u=44b2ca89ff52f128d609cb138&amp;id=f58c83fc50"
-          method="post"
-          id="mc-embedded-subscribe-form"
-          name="mc-embedded-subscribe-form"
-          target="_blank"
-          novalidate
-          className="flex flex-wrap lg:flex-no-wrap space-y-2 sm:space-y-0 justify-center lg:justify-start sm:space-x-4 validate"
-        >
-          <input
-            placeholder="Your Email"
-            className="border-2 border-gray-300 rounded-md w-full sm:w-64 pl-3 py-2"
-            type="email"
-            name="EMAIL"
-            id="mce-EMAIL"
-            required
-          />
-              {/* <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true"><input type="text" name="b_44b2ca89ff52f128d609cb138_f58c83fc50" tabindex="-1" value="" /></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" /></div> */}
-          <button type="submit" className="bg-pink-500 text-white rounded-md py-2 px-6 w-full sm:w-auto">
-            Subscribe
-          </button>
-        </form>
+        <SimpleForm />
+        <MailchimpSubscribe
+          url={url}
+          render={({ subscribe, status, message }) => (
+            <form
+              onSubmit={(formData) => subscribe(formData)}
+              method="post"
+              id="mc-embedded-subscribe-form"
+              name="mc-embedded-subscribe-form"
+              target="_blank"
+              novalidate
+              className="flex flex-wrap lg:flex-no-wrap space-y-2 sm:space-y-0 justify-center lg:justify-start sm:space-x-4 validate"
+            >
+              {status === "sending" && (
+                <div style={{ color: "blue" }}>sending...</div>
+              )}
+              {status === "error" && (
+                <div
+                  style={{ color: "red" }}
+                  dangerouslySetInnerHTML={{ __html: message }}
+                />
+              )}
+              {status === "success" && (
+                <div style={{ color: "green" }}>Subscribed !</div>
+              )}
+              <input
+                placeholder="Your Email"
+                className="border-2 border-gray-300 rounded-md w-full sm:w-64 pl-3 py-2"
+                type="email"
+                name="EMAIL"
+                id="mce-EMAIL"
+                required
+              />
+              <button
+                type="submit"
+                className="bg-pink-500 text-white rounded-md py-2 px-6 w-full sm:w-auto"
+              >
+                Subscribe
+              </button>
+            </form>
+          )}
+        />
       </div>
     );
 }
