ca33195088a93bfa37a1b78e353311c5f9cb6b4f
TNORTHERN <traynorthern@yahoo.com>
2020-07-06 11:56:17 -0500

    reverting footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div
-      className="sm:h-56 py-8 lg:py-0 lg:absolute bottom-0 w-full"
+      className="lg:h-56 py-8 lg:py-0 lg:absolute bottom-0 w-full"
       style={{
         backgroundImage: `url(${require("../../assets/bgblue.jpg")})`,
       }}
